817c0dd7008a5e749a23b59baf41c00a34e8a274
Trent McHugh <gnosislaunceston@gmail.com>
2026-06-01 16:37:43 +1000

    Fix redirect issue: trailingSlash never, consistent canonicals and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { imagetools } from 'vite-imagetools'
 export default defineConfig({
   site: 'https://gnosistasmania.com.au',
   output: 'static',
+  trailingSlash: 'never',
   integrations: [react()],
   vite: {
     plugins: [imagetools()],
